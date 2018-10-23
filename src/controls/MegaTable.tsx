90d421505fefd6856cdbaeaa59dd78ef04be7723
WilliamNadeau <33641839+WilliamNadeau@users.noreply.github.com>
2018-10-22 20:16:29 -0500

    new style edit stuff

--- src/controls/MegaTable.tsx
+++ src/controls/MegaTable.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import AddBox from '../controls/AddBox';
-import DeleteButton from '../controls/DeleteButton';
-import UpDown from '../controls/UpDown';
 
 type MegaTableProps<T> = {
   items: T[],
   render: (index: number, item: T) => JSX.Element[],
   keySelector: (item: T) => string,
-  add: (value: String) => void,
-  remove: (removed: T) => void,
-  move: (fromIndex: number, toIndex: number) => void,
+  add?: (value: String) => void,
+  more?: (item: T) => void,
+  remove?: (removed: T) => void,
+  move?: (fromIndex: number, toIndex: number) => void,
   className?: string
 };
 
@@ -19,7 +18,7 @@ class MegaTable<T> extends React.Component<MegaTableProps<T>> {
   }
 
   render() {
-    const { items, add, remove, move, className, render, keySelector } = this.props;
+    const { items, add, remove, more, move, className, render, keySelector } = this.props;
 
     return (
       <div className={className}>
@@ -27,25 +26,42 @@ class MegaTable<T> extends React.Component<MegaTableProps<T>> {
           className="list-group">
           {items && items.map((item, index) => (
             <div key={keySelector(item)}
+              style={{ zIndex: 5 }}
               className="list-group-item d-flex align-items-center">
 
               {render(index, item)}
 
-              <div className="pl-2 hide-unless-hover">
-                <DeleteButton onDelete={() => remove(item)} />
-              </div>
-              <div className="pl-2 hide-unless-hover">
-                <UpDown
-                  onUp={() => move(index, index - 1)}
-                  onDown={() => move(index, index + 1)} />
+              <div className="pr-2 position-absolute hide-unless-hover"
+                style={{ right: '0', bottom: '-20px', zIndex: 99 }}>
+                <div className="m-2 btn-group">
+                  {more ? <button className="btn btn-info btn-sm" onClick={evt => {
+                    evt.preventDefault();
+                    more(item);
+                  }}>⋯</button> : undefined}
+                  {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
+                    evt.preventDefault();
+                    move(index, index + 1);
+                  }}>↓</button> : undefined}
+                  {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
+                    evt.preventDefault();
+                    move(index, index - 1);
+                  }}>↑</button> : undefined}
+                  {remove ? <button className="btn btn-danger btn-sm" onClick={evt => {
+                    evt.preventDefault();
+                    remove(item);
+                  }}>✘</button> : undefined}
+                </div>
               </div>
             </div>
           ))}
         </div>
 
-        <AddBox placeholder="add ability" onAdd={value => add(value)} />
+        {add ? <AddBox placeholder="add ability" onAdd={value => add(value)} /> : undefined}
       </div>);
   }
 }
+
+// <button className="btn btn-primary btn-sm">＋</button>
+// <button className="btn btn-success btn-sm">✓</button>
 
 export default MegaTable;
