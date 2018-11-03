3d532ba6dd39e15a9fcfe293874d646294038395
Invalidator <william.w.nadeau@gmail.com>
2018-11-03 10:58:15 -0500

    updated megatable

--- src/controls/MegaTable.tsx
+++ src/controls/MegaTable.tsx
@@ -4,56 +4,82 @@ import AddBox from '../controls/AddBox';
 type MegaTableProps<T> = {
   items: T[],
   render: (index: number, item: T) => JSX.Element[],
-  keySelector: (item: T) => string,
+  keySelector?: (item: T) => string,
   add?: (value: String) => void,
   more?: (item: T) => void,
+  expand?: (item: T) => JSX.Element,
   remove?: (removed: T) => void,
   move?: (fromIndex: number, toIndex: number) => void,
   className?: string
 };
 
-class MegaTable<T> extends React.Component<MegaTableProps<T>> {
+class MegaTable<T> extends React.Component<MegaTableProps<T>, { expanded: string[] }> {
   constructor(props: MegaTableProps<T>) {
     super(props);
+
+    this.toggleExpansion = this.toggleExpansion.bind(this);
+
+    this.state = { expanded: [] };
+  }
+
+  toggleExpansion(key: string) {
+
+    const expanded = this.state.expanded.indexOf(key) >= 0;
+
+    this.setState(expanded ? 
+      { expanded: this.state.expanded.filter(e => e !== key) } : 
+      { expanded: [...this.state.expanded, key] });
   }
 
   render() {
-    const { items, add, remove, more, move, className, render, keySelector } = this.props;
+    const { items, add, remove, more, expand, move, className, render, keySelector } = this.props;
 
     return (
       <div className={className}>
         <div
           className="list-group">
-          {items && items.map((item, index) => (
-            <div key={keySelector(item)}
-              style={{ zIndex: 5 + items.length - index }}
-              className="list-group-item d-flex align-items-center">
+          {items && items.map((item, index) => {
 
-              {render(index, item)}
+            const key = keySelector ? keySelector(item) : index.toString();
+            const expanded = this.state.expanded.indexOf(key) >= 0;
 
-              <div className="pr-2 position-absolute hide-unless-hover"
-                style={{ right: '0', bottom: '-20px', zIndex: 99 }}>
-                <div className="m-2 btn-group">
-                  {more ? <button className="btn btn-info btn-sm" onClick={evt => {
-                    evt.preventDefault();
-                    more(item);
-                  }}>⋯</button> : undefined}
-                  {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
-                    evt.preventDefault();
-                    move(index, index + 1);
-                  }}>↓</button> : undefined}
-                  {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
-                    evt.preventDefault();
-                    move(index, index - 1);
-                  }}>↑</button> : undefined}
-                  {remove ? <button className="btn btn-danger btn-sm" onClick={evt => {
-                    evt.preventDefault();
-                    remove(item);
-                  }}>✘</button> : undefined}
+            return (
+              <div key={key}
+                style={{ zIndex: 5 + items.length - index }}
+                className="list-group-item d-flex align-items-center">
+
+                {render(index, item)}
+
+                {expand && expanded ? expand(item) : undefined}
+
+                <div className="pr-2 position-absolute hide-unless-hover"
+                  style={{ right: '0', bottom: '-20px', zIndex: 99 }}>
+                  <div className="m-2 btn-group">
+                    {expand ? <button className="btn btn-info btn-sm" onClick={evt => {
+                      evt.preventDefault();
+                      this.toggleExpansion(key);
+                    }}>+</button> : undefined}
+                    {more ? <button className="btn btn-info btn-sm" onClick={evt => {
+                      evt.preventDefault();
+                      more(item);
+                    }}>⋯</button> : undefined}
+                    {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
+                      evt.preventDefault();
+                      move(index, index + 1);
+                    }}>↓</button> : undefined}
+                    {move ? <button className="btn btn-secondary btn-sm" onClick={evt => {
+                      evt.preventDefault();
+                      move(index, index - 1);
+                    }}>↑</button> : undefined}
+                    {remove ? <button className="btn btn-danger btn-sm" onClick={evt => {
+                      evt.preventDefault();
+                      remove(item);
+                    }}>✘</button> : undefined}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         {add ? <AddBox placeholder="add ability" onAdd={value => add(value)} /> : undefined}
--- src/sheetManage/EffectsPanel.tsx
+++ src/sheetManage/EffectsPanel.tsx
@@ -2,10 +2,8 @@ import * as React from 'react';
 import Sheet, { Effect, Target } from '../sheet/SheetModel';
 import SheetPanel from './SheetPanel';
 import InlineEdit from '../controls/InlineEdit';
-import AddBox from '../controls/AddBox';
-import DeleteButton from '../controls/DeleteButton';
 import FormulaInlineEdit from '../controls/FormulaInlineEdit';
-import UpDown from '../controls/UpDown';
+import MegaTable from '../controls/MegaTable';
 
 type EffectsPanelProps = {
   className?: string,
@@ -18,101 +16,16 @@ type EffectsPanelProps = {
   reorder: (from: number, to: number) => void;
 };
 
-class EffectsPanel extends React.Component<EffectsPanelProps, { expanded: string[] }> {
+class EffectTable extends MegaTable<Effect> { }
+class TargetTable extends MegaTable<Target> { }
+
+class EffectsPanel extends React.Component<EffectsPanelProps> {
   constructor(props: EffectsPanelProps) {
     super(props);
-
-    this.state = { expanded: [] };
-  }
-
-  toRow = (sheet: Sheet, effect: Effect, index: number) => {
-    const isActive = effect.active;
-
-    const expanded = this.state.expanded.find(f => effect.name === f) ? true : false;
-
-    var rows = [(
-      <div key={effect.name}
-        className="list-group-item d-flex align-items-center">
-        <div style={{ width: '100%' }}>
-          <InlineEdit
-            priorValue={effect.name}
-            onChange={v => this.props.updateEffect(index, effect.updateName(v))} />
-        </div>
-        <div className="pl-2">
-          <button
-            className={'btn btn-small ' + (isActive ? 'btn-primary' : '')}
-            onClick={event => {
-              event.preventDefault();
-              isActive ? this.props.inactivateEffect(effect) : this.props.activateEffect(effect);
-            }}>
-            {isActive ? 'ACTIVE' : 'INACTIVE'}
-          </button>
-        </div>
-        <div className="pl-2 hide-unless-hover">
-          <button
-            onClick={evt => {
-              evt.preventDefault();
-              expanded ?
-                this.setState({ expanded: [...this.state.expanded.filter(f => f !== effect.name)] }) :
-                this.setState({ expanded: [...this.state.expanded, effect.name] });
-            }}
-            className="btn btn-primary">{expanded ? '↑' : '↓'}</button>
-        </div>
-        <div className="pl-2 hide-unless-hover"><DeleteButton onDelete={() => this.props.deleteEffect(effect)} /></div>
-        <div className="pl-2 hide-unless-hover">
-          <UpDown
-            onUp={() => this.props.reorder(index, index - 1)}
-            onDown={() => this.props.reorder(index, index + 1)} />
-        </div>
-      </div>
-    )];
-
-    if (expanded) {
-      rows = rows.concat(...effect.targets.map((target, targetIndex) => (
-        <div
-          key={effect.name + target.statisticName}
-          className="ml-4 list-group-item d-flex align-items-center">
-          <div style={{ width: '100%' }}>
-            <div>
-              <InlineEdit
-                priorValue={target.statisticName}
-                onChange={n => this.props.updateEffect(
-                  index,
-                  effect.updateTarget(targetIndex, target.updateName(n)))} />
-            </div>
-            <div>
-              <FormulaInlineEdit
-                className="text-muted small"
-                priorFormula={target.formula}
-                sheet={sheet}
-                onChange={n => this.props.updateEffect(
-                  index,
-                  effect.updateTarget(targetIndex, target.updateFormula(n)))} />
-            </div>
-          </div>
-          <div className="pl-2">
-            <DeleteButton onDelete={() => this.props.updateEffect(index, effect.deleteTarget(target))} />
-          </div>
-        </div>
-      )));
-      rows.push((
-        <div
-          key={effect.name + 'addTarget'}
-          className="ml-4 list-group-item d-flex align-items-center">
-          <AddBox
-            placeholder="add target"
-            onAdd={statisticName => this.props.updateEffect(
-              index,
-              effect.addTarget(new Target({ statisticName })))} />
-        </div>
-      ));
-    }
-
-    return rows;
   }
 
   render() {
-    const { sheet, className, addEffect } = this.props;
+    const { sheet, className } = this.props;
     const effects = sheet.effects;
 
     return (
@@ -120,11 +33,68 @@ class EffectsPanel extends React.Component<EffectsPanelProps, { expanded: string
         title="Effects"
         className={className}>
 
-        <div className="list-group">
+        {/* <div className="list-group">
           {effects.map((effect, index) => this.toRow(sheet, effect, index))}
         </div>
 
-        <AddBox placeholder="add effect" onAdd={name => addEffect(new Effect({ name }))} />
+        <AddBox placeholder="add effect" onAdd={name => addEffect(new Effect({ name }))} /> */}
+
+        <EffectTable
+          items={effects}
+          keySelector={e => e.name}
+          add={name => this.props.addEffect(new Effect({ name }))}
+          remove={effect => this.props.deleteEffect(effect)}
+          move={(from, to) => this.props.reorder(from, to)}
+          render={(index, effect) => [
+            (
+              <div style={{ width: '100%' }}>
+                <InlineEdit
+                  priorValue={effect.name}
+                  onChange={v => this.props.updateEffect(index, effect.updateName(v))} />
+              </div>
+            ),
+            (
+              <div className="pl-2">
+                <button
+                  className={'btn btn-small ' + (effect.active ? 'btn-primary' : '')}
+                  onClick={event => {
+                    event.preventDefault();
+                    effect.active ? this.props.inactivateEffect(effect) : this.props.activateEffect(effect);
+                  }}>
+                  {effect.active ? 'ACTIVE' : 'INACTIVE'}
+                </button>
+              </div>
+            )
+          ]}
+          expand={effect => (
+            <TargetTable
+              key={effect.name + 'expando'}
+              items={effect.targets}
+              render={(index, target) => [
+                (
+                  <div>
+                    <InlineEdit
+                      priorValue={target.statisticName}
+                      onChange={n => this.props.updateEffect(
+                        index,
+                        effect.updateTarget(index, target.updateName(n)))} />
+                  </div>
+                ),
+                (
+                  <div>
+                    <FormulaInlineEdit
+                      className="text-muted small"
+                      priorFormula={target.formula}
+                      sheet={sheet}
+                      onChange={n => this.props.updateEffect(
+                        index,
+                        effect.updateTarget(index, target.updateFormula(n)))} />
+                  </div>
+                )
+              ]}
+            />
+          )}
+        />
 
       </SheetPanel>
     );
